4d565b896f7c3351b01f1342f19e30115a437c14
d3x0r <d3x0r@github.com>
2016-11-29 14:05:27 -0800

    updated version; add missing source

---
+++ vfs_module.cc
@@ -0,0 +1,549 @@
+
+
+#include <node.h>
+#include <node_object_wrap.h>
+#include <v8.h>
+//#include <nan.h>
+
+#include "src/sack.h"
+#undef New
+
+using namespace v8;
+
+static struct local {
+   PLIST volumes;
+} l;
+
+class VolumeObject : node::ObjectWrap {
+public:
+	struct volume *vol;
+	static v8::Persistent<v8::Function> constructor;
+	
+public:
+
+	static void Init( Handle<Object> exports );
+	VolumeObject( const char *mount, const char *filename, const char *key, const char *key2 );
+
+	static void New( const FunctionCallbackInfo<Value>& args );
+
+   ~VolumeObject();
+};
+
+class SqlObject : node::ObjectWrap {
+public:
+	PODBC odbc;
+   int optionInitialized;
+	static v8::Persistent<v8::Function> constructor;
+	int columns;
+	CTEXTSTR *result;
+	CTEXTSTR *fields;
+	Persistent<Object> volume;
+public:
+
+	static void Init( Handle<Object> exports );
+	SqlObject( const char *dsn, Isolate* isolate, Local<Object> o );
+
+	static void New( const FunctionCallbackInfo<Value>& args );
+
+	static void query( const FunctionCallbackInfo<Value>& args );
+	static void option( const FunctionCallbackInfo<Value>& args );
+	static void setOption( const FunctionCallbackInfo<Value>& args );
+	static void makeTable( const FunctionCallbackInfo<Value>& args );
+
+   ~SqlObject();
+};
+
+
+class FileObject : public node::ObjectWrap {
+	struct sack_vfs_file *file;
+   //Local<Object> volume;
+   Persistent<Object> volume;
+public:
+	static v8::Persistent<v8::Function> constructor;
+	static void Init(  );
+
+	static void openFile( const FunctionCallbackInfo<Value>& args );
+	static void readFile( const FunctionCallbackInfo<Value>& args );
+	static void writeFile( const FunctionCallbackInfo<Value>& args );
+	static void seekFile( const FunctionCallbackInfo<Value>& args );
+
+	//static void readFile( const FunctionCallbackInfo<Value>& args );
+
+	static void Emitter( const FunctionCallbackInfo<Value>& args );
+
+	FileObject( VolumeObject* vol, const char *filename, Isolate*, Local<Object> o );
+   ~FileObject();
+};
+
+static void moduleExit( void *arg ) {
+	InvokeExits();
+}
+
+void VolumeObject::Init( Handle<Object> exports ) {
+	InvokeDeadstart();
+	node::AtExit( moduleExit );
+	{
+		//extern void Syslog
+	}
+		Isolate* isolate = Isolate::GetCurrent();
+		Local<FunctionTemplate> volumeTemplate;
+		FileObject::Init();
+		SqlObject::Init( exports );
+		// Prepare constructor template
+		volumeTemplate = FunctionTemplate::New( isolate, New );
+		volumeTemplate->SetClassName( String::NewFromUtf8( isolate, "Volume" ) );
+		volumeTemplate->InstanceTemplate()->SetInternalFieldCount( 2 );
+
+		// Prototype
+		NODE_SET_PROTOTYPE_METHOD( volumeTemplate, "File", FileObject::openFile );
+		//NODE_SET_PROTOTYPE_METHOD( volumeTemplate, "Sqlite", SqlObject::New );
+
+		constructor.Reset( isolate, volumeTemplate->GetFunction() );
+		exports->Set( String::NewFromUtf8( isolate, "Volume" ),
+			volumeTemplate->GetFunction() );
+	}
+
+VolumeObject::VolumeObject( const char *mount, const char *filename, const char *key, const char *key2 )  {
+	vol = sack_vfs_load_crypt_volume( filename, key, key2 );
+	sack_mount_filesystem( mount, sack_get_filesystem_interface( SACK_VFS_FILESYSTEM_NAME )
+			, 2000, (uintptr_t)vol, TRUE );
+}
+
+
+void logBinary( char *x, int n )
+{
+	int m;
+	for( m = 0; m < n; ) {
+		int o;
+		for( o = 0; o < 16 && m < n; o++, m++ ) {
+			printf( "%02x ", x[m] );
+		}
+		m -= o;
+		for( o = 0; o < 16 && m < n; o++, m++ ) {
+			printf( "%c", (x[m]>32 && x[m]<127)?x[m]:'.' );
+		}
+	}
+}
+
+	void VolumeObject::New( const FunctionCallbackInfo<Value>& args ) {
+		Isolate* isolate = args.GetIsolate();
+		if( args.IsConstructCall() ) {
+			char *mount_name;
+			char *filename = "default.vfs";
+			char *key = NULL;
+			char *key2 = NULL;
+			int argc = args.Length();
+			if( argc > 0 ) {
+				String::Utf8Value fName( args[0]->ToString() );
+				mount_name = StrDup( *fName );
+			}
+			if( argc > 1 ) {
+				String::Utf8Value fName( args[1]->ToString() );
+				filename = StrDup( *fName );
+			}
+			if( argc > 2 ) {
+				String::Utf8Value k( args[2] );
+				key = StrDup( *k );
+			}
+			if( argc > 3 ) {
+				String::Utf8Value k( args[3] );
+				key2 = StrDup( *k );
+			}
+			// Invoked as constructor: `new MyObject(...)`
+			VolumeObject* obj = new VolumeObject( mount_name, filename, key, key2 );
+			obj->Wrap( args.This() );
+			args.GetReturnValue().Set( args.This() );
+
+			Deallocate( char*, mount_name );
+			Deallocate( char*, filename );
+			Deallocate( char*, key );
+			Deallocate( char*, key2 );
+		}
+		else {
+			// Invoked as plain function `MyObject(...)`, turn into construct call.
+			int argc = args.Length();
+		   Local<Value> *argv = new Local<Value>[argc];
+			for( int n = 0; n < argc; n++ )
+            argv[n] = args[n];
+
+			Local<Function> cons = Local<Function>::New( isolate, constructor );
+			args.GetReturnValue().Set( cons->NewInstance( argc, argv ) );
+         delete argv;
+		}
+	}
+
+
+void FileObject::Emitter(const FunctionCallbackInfo<Value>& args)
+{
+	Isolate* isolate = Isolate::GetCurrent();
+	//HandleScope scope;
+	Handle<Value> argv[2] = {
+		v8::String::NewFromUtf8( isolate, "ping"), // event name
+		args[0]->ToString()  // argument
+	};
+
+	node::MakeCallback(isolate, args.This(), "emit", 2, argv);
+}
+
+
+void FileObject::readFile(const FunctionCallbackInfo<Value>& args) {
+
+}
+
+void FileObject::writeFile(const FunctionCallbackInfo<Value>& args) {
+
+}
+
+void FileObject::seekFile(const FunctionCallbackInfo<Value>& args) {
+
+}
+
+
+	void FileObject::Init(  ) {
+		Isolate* isolate = Isolate::GetCurrent();
+
+		Local<FunctionTemplate> fileTemplate;
+		// Prepare constructor template
+		fileTemplate = FunctionTemplate::New( isolate, openFile );
+		fileTemplate->SetClassName( String::NewFromUtf8( isolate, "File" ) );
+		fileTemplate->InstanceTemplate()->SetInternalFieldCount( 13 );
+
+		// Prototype
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "read", readFile );
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "write", writeFile );
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "seek", seekFile );
+		// read stream methods
+		//NODE_SET_PROTOTYPE_METHOD( fileTemplate, "dir", getDirectory );
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "pause", openFile );
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "resume", openFile );
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "setEncoding", openFile );
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "unpipe", openFile );
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "unshift", openFile );
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "wrap", openFile );
+		// write stream methods
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "cork", openFile );
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "uncork", openFile );
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "end", openFile );
+		NODE_SET_PROTOTYPE_METHOD( fileTemplate, "setDefaultEncoding", openFile );
+
+		//NODE_SET_PROTOTYPE_METHOD( fileTemplate, "isPaused", openFile );
+
+		constructor.Reset( isolate, fileTemplate->GetFunction() );
+		//exports->Set( String::NewFromUtf8( isolate, "File" ),
+		//	fileTemplate->GetFunction() );
+	}
+
+	void FileObject::openFile( const FunctionCallbackInfo<Value>& args ) {
+		Isolate* isolate = args.GetIsolate();
+		if( args.Length() < 1 ) {
+			isolate->ThrowException( Exception::TypeError(
+				String::NewFromUtf8( isolate, "Requires filename to open" ) ) );
+			return;
+		}
+		VolumeObject* vol;
+		char *filename;
+		String::Utf8Value fName( args[0] );
+		filename = *fName;
+
+		if( args.IsConstructCall() ) {
+			// Invoked as constructor: `new MyObject(...)`
+			FileObject* obj;
+			if( args.Length() < 2 ) {
+				vol = ObjectWrap::Unwrap<VolumeObject>( args.Holder() );
+				obj = new FileObject( vol, filename, isolate, args.Holder() );
+			}
+			else {
+				vol = ObjectWrap::Unwrap<VolumeObject>( args[1]->ToObject() );
+				obj = new FileObject( vol, filename, isolate, args[1]->ToObject() );
+			}
+			obj->Wrap( args.This() );
+			args.GetReturnValue().Set( args.This() );
+		}
+		else {
+			const int argc = 2;
+			Local<Value> argv[argc] = { args[0], args.Holder() };
+			Local<Function> cons = Local<Function>::New( isolate, constructor );
+			args.GetReturnValue().Set( cons->NewInstance( argc, argv ) );
+		}
+	}
+
+	FileObject::FileObject( VolumeObject* vol, const char *filename, Isolate* isolate, Local<Object> o ) : 
+		volume( isolate, o )
+	{
+		file = sack_vfs_openfile( vol->vol, filename );
+	}
+
+
+Persistent<Function> VolumeObject::constructor;
+Persistent<Function> FileObject::constructor;
+Persistent<Function> SqlObject::constructor;
+
+VolumeObject::~VolumeObject() {
+	//printf( "Volume object evaporated.\n" );
+	sack_vfs_unload_volume( vol );
+}
+
+FileObject::~FileObject() {
+	//printf( "File object evaporated.\n" );
+	sack_vfs_close( file );
+	volume.Reset();
+}
+
+
+//-----------------------------------------------------------
+//   SQL Object
+//-----------------------------------------------------------
+
+
+void SqlObject::Init( Handle<Object> exports ) {
+	Isolate* isolate = Isolate::GetCurrent();
+
+	Local<FunctionTemplate> sqlTemplate;
+	// Prepare constructor template
+	sqlTemplate = FunctionTemplate::New( isolate, New );
+	sqlTemplate->SetClassName( String::NewFromUtf8( isolate, "Sqlite" ) );
+	sqlTemplate->InstanceTemplate()->SetInternalFieldCount( 7 );  // have to add 1 implicit constructor.
+
+	// Prototype
+	NODE_SET_PROTOTYPE_METHOD( sqlTemplate, "do", query );
+	NODE_SET_PROTOTYPE_METHOD( sqlTemplate, "op", option );
+	NODE_SET_PROTOTYPE_METHOD( sqlTemplate, "getOption", option );
+	//NODE_SET_PROTOTYPE_METHOD( sqlTemplate, "so", setOption );
+	NODE_SET_PROTOTYPE_METHOD( sqlTemplate, "so", setOption );
+	NODE_SET_PROTOTYPE_METHOD( sqlTemplate, "setOption", setOption );
+	//NODE_SET_PROTOTYPE_METHOD( sqlTemplate, "makeTable", makeTable );
+	NODE_SET_PROTOTYPE_METHOD( sqlTemplate, "makeTable", makeTable );
+	constructor.Reset( isolate, sqlTemplate->GetFunction() );
+	exports->Set( String::NewFromUtf8( isolate, "Sqlite" ),
+		sqlTemplate->GetFunction() );
+}
+
+//-----------------------------------------------------------
+
+void SqlObject::New( const FunctionCallbackInfo<Value>& args ) {
+	Isolate* isolate = args.GetIsolate();
+	if( args.IsConstructCall() ) {
+		char *dsn;
+		String::Utf8Value arg( args[0] );
+		dsn = *arg;
+		VolumeObject* vol;
+		SqlObject* obj;
+		if( args.Length() < 2 ) {
+			obj = new SqlObject( dsn, isolate, args.Holder() );
+		}
+		else {
+			obj = new SqlObject( dsn, isolate, args[1]->ToObject() );
+		}
+		obj->Wrap( args.This() );
+		args.GetReturnValue().Set( args.This() );
+	} else {
+		const int argc = 2;
+		Local<Value> argv[argc] = { args[0], args.Holder() };
+		Local<Function> cons = Local<Function>::New( isolate, constructor );
+		args.GetReturnValue().Set( cons->NewInstance( argc, argv ) );
+	}
+}
+
+//-----------------------------------------------------------
+
+void SqlObject::query( const FunctionCallbackInfo<Value>& args ) {
+	Isolate* isolate = args.GetIsolate();
+
+	char *query;
+	String::Utf8Value tmp( args[0] );
+	query = StrDup( *tmp );
+
+
+	SqlObject *sql = ObjectWrap::Unwrap<SqlObject>( args.This() );
+	sql->fields = 0;
+	if( !SQLRecordQuery( sql->odbc, query, &sql->columns, &sql->result, &sql->fields ) ) {
+		const char *error;
+		FetchSQLError( sql->odbc, &error );
+		isolate->ThrowException( Exception::Error(
+			String::NewFromUtf8( isolate, error ) ) );
+		return;
+	}
+	if( sql->columns )
+	{
+		Local<Array> records = Array::New( isolate );
+		Local<Object> record = Object::New( isolate );
+		if( sql->result ) {
+			int row = 0;
+			do {
+				record = Object::New( isolate );
+				for( int n = 0; n < sql->columns; n++ ) {
+					record->Set( String::NewFromUtf8( isolate, sql->fields[n] )
+						, String::NewFromUtf8( isolate, sql->result[n] ) );
+				}
+				records->Set( row++, record );
+			} while( FetchSQLRecord( sql->odbc, &sql->result ) );
+		}
+		args.GetReturnValue().Set( records );
+	}
+	else
+	{
+		SQLEndQuery( sql->odbc );
+		args.GetReturnValue().Set( True( isolate ) );
+	}
+	Deallocate( char*, query );
+}
+
+//-----------------------------------------------------------
+
+
+SqlObject::SqlObject( const char *dsn, Isolate* isolate, Local<Object> o ) :
+	volume( isolate, o )
+{
+   odbc = ConnectToDatabase( dsn );
+   optionInitialized = FALSE;
+}
+
+SqlObject::~SqlObject() {
+	CloseDatabase( odbc );
+	volume.Reset();
+}
+
+//-----------------------------------------------------------
+
+void SqlObject::option( const FunctionCallbackInfo<Value>& args ) {
+	Isolate* isolate = args.GetIsolate();
+
+	int argc = args.Length();
+	char *sect;
+	char *optname;
+	char *defaultVal;
+
+	if( argc > 0 ) {
+		String::Utf8Value tmp( args[0] );
+		defaultVal = StrDup( *tmp );
+	}
+	else
+		defaultVal = "";
+
+	if( argc > 1 ) {
+		String::Utf8Value tmp( args[1] );
+		sect = defaultVal;
+		defaultVal = StrDup( *tmp );
+	}
+	else
+		sect = NULL;
+
+	if( argc > 2 ) {
+		String::Utf8Value tmp( args[2] );
+		optname = defaultVal;
+		defaultVal = StrDup( *tmp );
+	}
+	else
+		optname = NULL;
+
+	TEXTCHAR readbuf[1024];
+
+	SqlObject *sql = ObjectWrap::Unwrap<SqlObject>( args.This() );
+	sql->fields = 0;
+
+	if( !sql->optionInitialized ) {
+		SetOptionDatabaseOption( sql->odbc, 2 );
+      sql->optionInitialized = TRUE;
+	}
+
+	SACK_GetPrivateProfileStringExxx( sql->odbc
+		, sect
+		, optname
+		, defaultVal
+		, readbuf
+		, 1024
+		, NULL
+		, TRUE
+		DBG_SRC
+		);
+
+	Local<String> returnval = String::NewFromUtf8( isolate, readbuf );
+	args.GetReturnValue().Set( returnval );
+
+	Deallocate( char*, optname );
+	Deallocate( char*, sect );
+	Deallocate( char*, defaultVal );
+}
+
+//-----------------------------------------------------------
+
+void SqlObject::setOption( const FunctionCallbackInfo<Value>& args ) {
+	Isolate* isolate = args.GetIsolate();
+
+	int argc = args.Length();
+	char *sect;
+	char *optname;
+	char *defaultVal;
+
+	if( argc > 0 ) {
+		String::Utf8Value tmp( args[0] );
+		defaultVal = StrDup( *tmp );
+	}
+	else
+		defaultVal = "";
+
+	if( argc > 1 ) {
+		String::Utf8Value tmp( args[1] );
+		sect = defaultVal;
+		defaultVal = StrDup( *tmp );
+	}
+	else
+		sect = NULL;
+
+	if( argc > 2 ) {
+		String::Utf8Value tmp( args[2] );
+		optname = defaultVal;
+		defaultVal = StrDup( *tmp );
+	}
+	else
+		optname = NULL;
+
+	TEXTCHAR readbuf[1024];
+
+	SqlObject *sql = ObjectWrap::Unwrap<SqlObject>( args.This() );
+	sql->fields = 0;
+
+	SACK_WriteOptionString( sql->odbc
+		, sect
+		, optname
+		, defaultVal
+	);
+
+	Local<String> returnval = String::NewFromUtf8( isolate, readbuf );
+	args.GetReturnValue().Set( returnval );
+
+	Deallocate( char*, optname );
+	Deallocate( char*, sect );
+	Deallocate( char*, defaultVal );
+}
+
+//-----------------------------------------------------------
+
+void SqlObject::makeTable( const FunctionCallbackInfo<Value>& args ) {
+	Isolate* isolate = args.GetIsolate();
+
+	int argc = args.Length();
+	char *tableCommand;
+
+	if( argc > 0 ) {
+		PTABLE table;
+		String::Utf8Value tmp( args[0] );
+		tableCommand = StrDup( *tmp );
+
+		SqlObject *sql = ObjectWrap::Unwrap<SqlObject>( args.This() );
+
+		table = GetFieldsInSQLEx( tableCommand, false DBG_SRC );
+		if( CheckODBCTable( sql->odbc, table, CTO_MERGE ) )
+
+			args.GetReturnValue().Set( True(isolate) );
+		args.GetReturnValue().Set( False( isolate ) );
+
+	}
+	//args.GetReturnValue().Set( returnval );
+	args.GetReturnValue().Set( False( isolate ) );
+}
+
+//-----------------------------------------------------------
+
+NODE_MODULE( vfs_module, VolumeObject::Init)
+
