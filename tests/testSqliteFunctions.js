0a4481c3239ac6a39788bb653863aa1a943b624a
d3x0r <d3x0r@no-reply.github.com>
2018-01-13 20:35:50 -0800

    go back to more generliazed parameter example

--- tests/testSqliteFunctions.js
+++ tests/testSqliteFunctions.js
@@ -12,9 +12,9 @@ console.log( db.do( "select * from sqlite_master" ) );
 if( db.makeTable( "create table table1 ( field ,val )" ) )
 	db.do( `insert into table1( field, val ) values ("a","b"),("c","d") ` );;
 
-db.function( "func", (a,b,c,d)=>{console.log( "passed", a,b,c,d ); return JSON.stringify( [a,b,c,d] ) } );
+db.function( "func", (...params)=>{console.log( "passed", params ); return JSON.stringify( params) } );
 db.aggregate( "agg"
-      , (...params)=>{console.log( "step:", ...params ) }
+      , (...params)=>{console.log( "step:", params ) }
       , ()=>{ console.log( "final"  ); return "abc" } );
 
 console.log( db.do( "select func(field,val,123,4.55) from table1" ) );
