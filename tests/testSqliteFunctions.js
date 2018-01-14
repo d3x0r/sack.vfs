910e39dff2407858abbd0382d14f1feb929c7bf9
d3x0r <d3x0r@no-reply.github.com>
2018-01-13 20:21:31 -0800

    Extend parameter test for functions

--- tests/testSqliteFunctions.js
+++ tests/testSqliteFunctions.js
@@ -9,13 +9,13 @@ db.do( `drop table table1` );;
 } catch( err) { }
 
 console.log( db.do( "select * from sqlite_master" ) );
-if( db.makeTable( "create table table1 ( field char,val char)" ) )
+if( db.makeTable( "create table table1 ( field ,val )" ) )
 	db.do( `insert into table1( field, val ) values ("a","b"),("c","d") ` );;
 
-db.function( "func", (...params)=>{console.log( "passed", ...params ); return params[0] } );
+db.function( "func", (a,b,c,d)=>{console.log( "passed", a,b,c,d ); return JSON.stringify( [a,b,c,d] ) } );
 db.aggregate( "agg"
       , (...params)=>{console.log( "step:", ...params ) }
       , ()=>{ console.log( "final"  ); return "abc" } );
 
-console.log( db.do( "select func(field,val) from table1" ) );
+console.log( db.do( "select func(field,val,123,4.55) from table1" ) );
 console.log( db.do( "select agg(val) from table1" ) );
