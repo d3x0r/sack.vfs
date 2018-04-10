c4ca7ef29cdbfd6277badb721bed09c103121785
d3x0r <d3x0r@no-reply.github.com>
2018-04-09 22:01:58 -0700

    Register json6 require loader

--- vfs_module.js
+++ vfs_module.js
@@ -17,30 +17,38 @@ try{
   } 
 }
 
-vfs.Sqlite.so( "SACK/Summoner/Auto register with summoner?", 0 );
 vfs.JSON6.stringify = JSON.stringify;
 vfs.JSON.stringify = JSON.stringify;
 
+var disk = sack.Volume();
+require.extensions['.json6'] = function (module, filename) {
+    var content = disk.read(filename).toString();
+    module.exports = sack.JSON6.parse(content);
+};
+
+
+//vfs.Sqlite.op( "SACK/Summoner/Auto register with summoner?", 0 );
 //vfs.Sqlite.so( "SACK/Summoner/Auto register with summoner?", 1 );
 //console.log( "register:", vfs.Sqlite.op( "SACK/Summoner/Auto register with summoner?", 0 ));
 //vfs.loadComplete();
-
-process.on( 'beforeExit', ()=> {
-	//console.log( "Before Exit." );
-	vfs.Thread() 
-});
-process.on( 'exit', ()=> {
-	//console.log( "Process Exit." );
-});
-process.on( 'uncaughtException', (err)=> {
-	console.log( err )	
-});
 const thread = vfs.Thread(process._tickDomainCallback);
-module.exports =exports= vfs;
+module.exports=exports=vfs;
 
-process.on('SIGUSR2',function(){
-        console.log('SIGUSR2 recieved')
-        //socketcluster.killWorkers();
-        //socketcluster.killBrokers();
-        //process.exit(1)
-      })
+if( false ) {
+	process.on( 'beforeExit', ()=> {
+		//console.log( "Before Exit." );
+		vfs.Thread() 
+	});
+	process.on( 'exit', ()=> {
+		//console.log( "Process Exit." );
+	});
+	process.on( 'uncaughtException', (err)=> {
+		console.log( err )	
+	});
+	process.on('SIGUSR2',function(){
+        	console.log('SIGUSR2 recieved')
+	        //socketcluster.killWorkers();
+        	//socketcluster.killBrokers();
+	        //process.exit(1)
+	})
+}
