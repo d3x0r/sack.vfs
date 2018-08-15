b842b4f904356c75d70c61b53d95a84ba8629dc6
d3x0r <d3x0r@no-reply.github.com>
2018-08-15 14:19:49 -0700

    For windows notepad has to wait a while because no end catcher in simplest test

--- tests/testTask.js
+++ tests/testTask.js
@@ -7,8 +7,12 @@ function logOutput(buf ) {
 
 if( process.platform === "linux" )
 	sack.Task( { bin: "cat", args:"testTask.js", input:logOutput } );
-else
+else {
 	sack.Task( { bin: "notepad.exe", args:"testTask.js" } );
+	setTimeout( ()=>{
+		console.log( "TICK? fault and end program?")
+	}, 5000 );
+}
 
 if(false)
 setTimeout( ()=>{
