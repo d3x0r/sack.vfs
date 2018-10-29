12448dc5643c782cbdb66219f1ede60880a7cae1
d3x0r <d3x0r@no-reply.github.com>
2018-10-28 17:52:35 -0700

    Improve object storage test

--- tests/objstore/testOs.js
+++ tests/objstore/testOs.js
@@ -1,35 +1,60 @@
 
 var sack = require( "../.." );
-sack.Volume().unlink( "container.vfs" );
+//sack.Volume().unlink( "container.vfs" );
 
 var vfs = sack.Volume( "cmount", "container.vfs" );
 //console.log( "sack:", sack );
 
 var store = sack.objectStorage( "cmount@storage.os" );
 
-
+sack.JSOX.parse( ` {data:{data:{key:"Q$Rs8jxhKL_ixzepmzlEArxFAT7UD1ZPaW7RM0QGtCA=",signed:[~os"ggolHF2N2JhIAROgIObAgbMnjyqUiBBoBBhFtAARYgA="],unsigned:[~os"VPRaPIXjol51rxYWMRkuK_uoBNA0CWkpDFcq6VP$f$0="]},nonce:null},id:"Q$Rs8jxhKL_ixzepmzlEArxFAT7UD1ZPaW7RM0QGtCA="} ` );
 
 var rootKey;
 
 var root = { key : null };
 
-vfs.readJSOX( "root", (root)=>{
-	rootKey = root.key;
-} );
-if( !rootKey ) {
-	var root;
+function initRoot() {
 	root.key = rootKey = store.put( root );
-	
+	root.unsigned = [];
+	root.signed = [];
+
 	vfs.write( "root", sack.JSOX.stringify( root ) );
+	addMoreNodes();
 }
-console.log( "Root Key:" , rootKey );
+
+if( !vfs.exists( "root" ) ) {
+	initRoot();
+}else
+vfs.readJSOX( "root", (newRoot)=>{
+	rootKey = newRoot.key;
+	if( !rootKey ) {
+		initRoot();
+	} else {
+		store.map( rootKey ).then( (obj)=>{
+			root = obj; 
+			console.log( "new root:", sack.JSOX.stringify(root,null,3) );
+			addMoreNodes();
+		} );
+		//console.log( "did get? " );
+	}
+	console.log( "Root Key:" , rootKey );
+} );
 
 
 //vfs.unlink( "storage.os" );
 
+function addMoreNodes() {
 
+console.log( "Root is now:", root );
+var obj;
+root.unsigned.push( obj = { hello:"world", root:root} );
+var id = store.put( obj );
+console.log( "GOT storage ID:", id );
+root.signed.push( obj = { hello:"world", root:root} );
+var id = store.put( obj, true );
+console.log( "GOT storage ID:", id );
 
-var id = store.put( { hello:"world", root:root} );
-console.log( "GOT:", id );
-var id = store.put( { hello:"world", root:root}, true );
-console.log( "GOT:", id );
+console.log( "THIS SHOULD REWRITE ROOT:", sack.JSOX.stringify( root,null,3) );
+store.put( root );
+
+}
