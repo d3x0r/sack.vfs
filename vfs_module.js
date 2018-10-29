05d318b1a7dbf5b7729c828e6a47520e0f4952a9
d3x0r <d3x0r@no-reply.github.com>
2018-10-28 17:58:32 -0700

    Added JSOX encode/decode interface to objectstorage; with automatic circular resolution

--- vfs_module.js
+++ vfs_module.js
@@ -39,24 +39,6 @@ var fromProtoTypes = new Map();
 sack.SaltyRNG.setSigningThreads( require( "os" ).cpus().length );
 
 
-function objectStorageContainer(o,sign) {
-	if( !this instanceof objectStorageContainer ) return new objectStorageContainer(o,sign);
-	this.data = {	
-			nonce : sign?sack.SaltyRNG.sign( sack.JSOX.stringify(o), 3, 3 ):null,
-			data : o
-		}
-	if( sign ) {
-		var v = sack.SaltyRNG.verify( sack.JSOX.stringify(o), this.data.nonce, 3, 3 );
-		//console.log( "TEST:", v );
-		this.id = v.key;
-		v.key = this.data.nonce;
-		this.data.nonce = v;
-	} else {
-		this.id = sack.id();
-	}
-	//console.log( "Container:", this );
-}
-
 //sack.Sqlite.op( "SACK/Summoner/Auto register with summoner?", 0 );
 //sack.Sqlite.so( "SACK/Summoner/Auto register with summoner?", 1 );
 //sack.loadComplete();
@@ -167,13 +149,16 @@ var JSOXBegin = sack.JSOX.begin;
 sack.JSOX.begin = function(cb) {
 	var parser = JSOXBegin( cb );
 	var localFromProtoTypes = new Map();;
+	var localPromiseFromProtoTypes = new Map();;
+	parser.setFromPrototypeMap( localFromProtoTypes );
+	parser.setPromiseFromPrototypeMap( localPromiseFromProtoTypes );
 	parser.registerFromJSOX = function( prototypeName, f ) {
 		if( localFromProtoTypes.get(prototypeName) ) throw new Error( "Existing fromJSOX has been registered for prototype" );
 		localFromProtoTypes.set( prototypeName, f );
 	}
 	parser.registerPromiseFromJSOX = function( prototypeName, f ) {
-		if( localFromProtoTypes.get(prototypeName) ) throw new Error( "Existing fromJSOX has been registered for prototype" );
-		localFromProtoTypes.set( prototypeName, f );
+		if( localPromiseFromProtoTypes.get(prototypeName) ) throw new Error( "Existing fromJSOX has been registered for prototype" );
+		localPromiseFromProtoTypes.set( prototypeName, f );
 	}
 	return parser;
 }
@@ -182,18 +167,57 @@ var arrayToJSOX;
 
 var _objectStorage = sack.objectStorage;
 
+function objectStorageContainer(o,sign) {
+	if( !this instanceof objectStorageContainer ) return new objectStorageContainer(o,sign);
+	this.data = {	
+			nonce : sign?sack.SaltyRNG.sign( sack.JSOX.stringify(o), 3, 3 ):null,
+			data : o
+		}
+	if( sign ) {
+		var v = sack.SaltyRNG.verify( sack.JSOX.stringify(o), this.data.nonce, 3, 3 );
+		//console.log( "TEST:", v );
+		this.id = v.key;
+		v.key = this.data.nonce;
+		this.data.nonce = v;
+	} else {
+		this.id = sack.id();
+	}
+	//console.log( "Container:", this );
+}
 
 sack.objectStorage.prototype.defineClasss = function(a,b) {
 	this.stringifier.defineClass(a,b);
 }
 
 sack.objectStorage.prototype.put = function( obj,sign ) {
+	
+	var container = this.stored.get( obj );
+	var storage;
+	//console.log( "Put found object?", container, obj );
+	if( container ) {
+		if( !container.nonce ) {
+			container = this.cachedContainer.get( container ); 
+			//console.log( "Container:", container );
+			storage = this.stringifier.stringify( container );
+			//console.log( "Update to:", container.id, storage );
+			this.write( container.id, storage );
+			return container.id;
+		} else { 
+			throw new Error( "record is signed" );
+		}
+	}
+	container = new objectStorageContainer(obj,sign);
 
-	var container = new objectStorageContainer(obj,sign);
 	//console.log( "saving stored container.id", obj, container.id );
-	this.stored.set( obj, container.id );
-	this.cached.set( container.id, container );
-	var storage = this.stringifier.stringify( container );
+
+	this.stored.delete( obj );
+	//this.stored.set( obj, container.id );
+	this.cached.set( container.id, container.data.data );
+	this.cachedContainer.set( container.id, container );
+	
+	storage = this.stringifier.stringify( container );
+
+	//console.log( "Create file:", container.id );
 	this.write( container.id, storage );
 	//console.log( "OUTPUT:", storage );
 	return container.id;
@@ -215,21 +239,123 @@ sack.objectStorage.prototype.get = function( key ) {
 	var resolve;
 	var reject;
 
-	this.read( key, (obj)=>{
-		if( obj )
-			resolve(obj);
-		else
-			reject();
-	} );
-	return new Promise( function(res,rej) {
+	function parserObject( obj ) {
+	}
+
+
+	var parser = sack.JSOX.begin( parserObject );
+	parser.registerFromJSOX( "~os", decodeStoredObjectKeyImmediate );
+	//console.log( "Something... " );
+
+	function decodeStoredObjectKey(objId,res,rej){
+		console.log( "Promised Reviver:", objId, this.mapping );
+
+		if( this.mapping ) {
+			var exist = this.cached.get( objId );
+			if( !exist ) {
+				console.log( "Chained get...",objid );
+				this.get( objId ).then( (obj)=>{
+					console.log( "Storage returned:", this, obj );
+					this.cached.set( objId, obj );
+					this.stored.set( obj, objId );
+					
+					res( obj );
+				} );
+			}
+			//console.log( "Otherwise returning existing:", exist );
+			//return exist;
+		} else {
+			console.log( "Returning existing" );
+			resolve( objId );
+			//return objId;
+		}
+	};
+
+	this.decoding.push( key );
+	function decodeStoredObjectKeyImmediate(objId,ref){
+		//console.log( "Revive:", objId, ref, this.mapping, this.decoding );
+		if( this.decoding.find( pending=>pending===objId ) ) {
+			//console.log( "Push a pending resolution for:",  {id:objId, ref: ref } );
+			this.pending.push( {id:objId, ref: ref } );
+			return objId;
+		}
+		this.decoding.push( objId );
+		if( this.mapping ) {
+			var exist = this.cached.get( objId );
+			if( !exist ) {
+				//console.log( "Chained get..." );
+
+				var parser = sack.JSOX.begin( parserObject );
+				parser.registerFromJSOX( "~os", decodeStoredObjectKeyImmediate );
+				//console.log( "Something.22.. ",objId );
+				this.read( objId, parser, (obj)=>{
+					//console.log( "Immediate result" );
+					// with a new parser, only a partial decode before revive again...
+					if( obj ){
+						Object.setPrototypeOf( obj, objectStorageContainer.prototype );
+						exist = obj.data.data;
+
+						this.stored.set( obj.data.data, obj.id );
+						this.cachedContainer.set( obj.id, obj ); 
+
+					}
+				} );
+				this.decoding.pop();
+				if( !this.decoding.length ) {
+					console.log( "So... I am?", sack.JSOX.stringify( obj ) );
+				}
+				var found;
+
+				do {
+					var found = this.pending.findIndex( pending=>pending.id === objId );
+					if( found >= 0 ) {
+						this.pending[found].ref.o[this.pending[found].ref.f] = exist;
+						this.pending.splice( found, 1 );
+					}
+				} while( found >= 0 );
+			}
+			//console.log( "Otherwise returning existing:", exist );
+			return exist;
+		} else {
+			//console.log( "Returning existing" );
+			resolve( objId );
+			return objId;
+		}
+	};
+
+	console.log( "Read Key:", key );
+	var p = new Promise( function(res,rej) {
 		resolve = res;  reject = rej;
 	} );
+	//console.log( "Read does exist..." );
+	this.read( key, parser, (obj)=>{
+		// with a new parser, only a partial decode before revive again...
+				var found;
+
+				do {
+					var found = this.pending.findIndex( pending=>pending.id === key );
+					if( found >= 0 ) {
+						this.pending[found].ref.o[this.pending[found].ref.f] = obj.data.data;
+						this.pending.splice( found, 1 );
+					}
+				} while( found >= 0 );
+
+		if( obj ){
+			Object.setPrototypeOf( obj, objectStorageContainer.prototype );
+			//console.log( "GOTzz:", obj );
+			this.stored.set( obj.data.data, obj.id );
+			this.cachedContainer.set( obj.id, obj ); 
+			
+			resolve(obj.data.data);
+		}else
+			reject();
+	} );
+
+
+	return p;
 }
 
 
-
-function parserObject( obj ) {
-}
 
 sack.objectStorage = function (...args) {
 	var mapping = false;
@@ -237,6 +363,8 @@ sack.objectStorage = function (...args) {
 	newStorage.cached = new Map();
 	newStorage.cachedContainer = new Map();
 	newStorage.stored = new WeakMap();
+	newStorage.decoding = [];
+	newStorage.pending = [];
 
 	newStorage.stringifier = sack.JSOX.stringifier();
 	function objectToJSOX(){
@@ -250,43 +378,30 @@ sack.objectStorage = function (...args) {
 			return '~os"'+exist+'"';
 		} else {			
 			if( this instanceof objectStorageContainer ) {
+				//console.log( "THIS SHOULD ALREADY BE IN THE STORAGE!", this, newStorage.stored.get( this.data.data ) );
+				//newStorage.stored.set( this.data.data, this.id );
+				//newStorage.cached.set( this.id, this.data.data );
+				//newStorage.cachedContainer.set( this.id, this );
 				newStorage.stored.set( this.data.data, this.id );
-				newStorage.cached.set( this.id, this.data.data );
-				newStorage.cachedContainer.set( this.id, this );
 			} else {
 				newStorage.cached.set( this.id, this );
 			}
-			newStorage.stored.set( this, this.id );
+			//console.log( "Commit as stored; first" );
 		}
 		return this;
 	}
 	newStorage.stringifier.setDefaultObjectToJSOX( objectToJSOX );
 	newStorage.stringifier.registerToJSOX( "~os", objectStorageContainer.prototype, objectToJSOX );
-	newStorage.parser = sack.JSOX.begin( parserObject );
 
-	newStorage.parser.registerPromiseFromJSOX( "~os", (objId,res,rej)=>{
-		if( newStorage.mapping ) {
-			var exist = newStorage.cached.get( objId );
-			if( !exist ) {
-				newStorage.get( objId ).then( (obj)=>{
-					newStorage.cached.set( objId, obj );
-					newStorage.stored.set( obj, objId );
-					res( obj );
-				} );
-			}
-		} else {
-			return objId;
-		}
-	} );
 
 	newStorage.map = function( expr ) {
-		mapping = true;
+		newStorage.mapping = true;
 		//this.parser.
 		var resolve;
 
-		this.get( key ).then( (obj)=>{
+		this.get( expr ).then( (obj)=>{
 			resolve(obj);
-			mapping = false;
+			newStorage.mapping = false;
 
 		} );
 		return new Promise( function(res,rej) {
