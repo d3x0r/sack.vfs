07e0ddbfea4ecea75e59d2df74e34c18d797e4ea
d3x0r <d3x0r@no-reply.github.com>
2019-02-11 13:45:52 -0800

    Fix some v8 deprecations

--- src/srg_module.cc
+++ src/srg_module.cc
@@ -31,7 +31,7 @@ private:
 				char *r;
 				struct random_context *ctx;
 				if( args.Length() > 1 && args[1]->IsNumber() )
-					version = args[1]->ToInt32( args.GetIsolate()->GetCurrentContext() ).ToLocalChecked()->Uint32Value();
+					version = args[1]->IntegerValue( args.GetIsolate()->GetCurrentContext() ).FromMaybe(0);
 				switch( version ) {
 				case 0:
 					ctx = SRG_CreateEntropy( NULL, 0 );
@@ -60,7 +60,7 @@ private:
 			else
 			{
 				if( args[0]->IsNumber() )
-					version = args[0]->ToInt32(args.GetIsolate()->GetCurrentContext()).ToLocalChecked()->Uint32Value();
+					version = args[0]->IntegerValue( args.GetIsolate()->GetCurrentContext() ).FromMaybe( 0 );
 
 				char *r;
 				switch( version ) {
