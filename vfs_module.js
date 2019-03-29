1de67faf4216a0f0f77f69f969a0b23ce720b853
d3x0r <d3x0r@github.com>
2019-03-28 23:22:31 -0700

    Remove noisy logging messages

--- vfs_module.js
+++ vfs_module.js
@@ -5,11 +5,11 @@ try{
   sack = require( "./build/RelWithDebInfo/sack_vfs.node" );
 } catch(err1) {
   try {
-    console.log( err1 );
+    //console.log( err1 );
     sack = require( "./build/Debug/sack_vfs.node" );
   } catch( err2 ){
     try {
-     console.log( err2 );
+      //console.log( err2 );
       sack = require( "./build/Release/sack_vfs.node" );
     } catch( err3 ){
       console.log( err1 )
