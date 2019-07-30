bc5109ad423c6b30183953403feb7ea3267fc0d6
d3x0r <d3x0r@github.com>
2019-07-29 23:19:40 -0700

    Test that pulling a directory doesn't triggr changes

--- tests/testFileMonitor.js
+++ tests/testFileMonitor.js
@@ -5,3 +5,14 @@ var monitor = new sack.FileMonitor( ".", 100 );
 monitor.addFilter( "*", (info)=>{
 	console.log( "Any File Changed:", info );
         } );
+
+var dir = sack.Volume().dir();
+for( var file in dir ) {
+	file = dir[file];
+	if( file.name == '.' || file.name == '..' ) continue;
+	if( file.folder ) {
+		var subdir = sack.Volume().dir( file.name )
+		console.log( "Sub?", subdir );
+	}
+}
+console.log( "DIR:", dir );
