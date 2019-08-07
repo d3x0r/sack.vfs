0da9b7910aed236ec21f7dff89fde18f9f53fc5e
d3x0r <d3x0r@no-reply.github.com>
2019-08-07 12:22:19 -0700

    use ffmpeg libraries to play sound instead of windows playsound

--- src/playSound_module.cc
+++ src/playSound_module.cc
@@ -1,12 +1,30 @@
 
 #include "global.h"
+#include <ffmpeg_interface.h>
 
+struct sound_cache_entry {
+	TEXTSTR name;
+	struct ffmpeg_file* file;
+};
+
+static struct sound_static_data {
+	PLIST sounds;
+};
+
+// ffmpeg_UnloadFile
+
+static void ended( uintptr_t sound_done ) {
+
+}
 
 static void playMedia( const v8::FunctionCallbackInfo<Value>& args ) {
 	if( args.Length() > 0 ) {
 		String::Utf8Value sound( args.GetIsolate(), args[0] );
-
-		PlaySound( *sound, NULL, SND_ASYNC );
+		uintptr_t endedData = 0;
+		struct ffmpeg_file* file = ffmpeg_LoadFile( *sound, NULL, 0, NULL, NULL, 0, ended, endedData, NULL );
+		ffmpeg_PlayFile( file );
+		ffmpeg_UnloadFile( file );
+		//PlaySound( *sound, NULL, SND_ASYNC );
 	}
 }
 
