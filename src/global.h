61f486ede8e6edba1998d6cdf52f0033e9e59be1
d3x0r <d3x0r@no-reply.github.com>
2019-11-05 03:49:44 -0800

    Provide defaults for optional arguments making strings

--- src/global.h
+++ src/global.h
@@ -36,6 +36,7 @@
 #include <http.h>
 #include <construct.h>
 #include <configscript.h>
+#include <filemon.h>
 #else
 #include "sack/sack.h"
 #endif
@@ -350,8 +351,8 @@ typedef struct arrayBufferHolder ARRAY_BUFFER_HOLDER, *PARRAY_BUFFER_HOLDER;
 DeclareSet( ARRAY_BUFFER_HOLDER );
 
 void releaseBuffer( const WeakCallbackInfo<ARRAY_BUFFER_HOLDER> &info );
-Local<String> localString( Isolate *isolate, const char *data, int len );
-Local<String> localStringExternal( Isolate *isolate, const char *data, int len, const char *real_root );
+Local<String> localString( Isolate *isolate, const char *data, int len = -1 );
+Local<String> localStringExternal( Isolate *isolate, const char *data, int len = -1, const char *real_root = NULL );
 
 void InitFS( const v8::FunctionCallbackInfo<Value>& args );
 void ConfigScriptInit( Local<Object> exports );
