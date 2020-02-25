5a8f6e2adf28138a82c28d397ae22e3c54d172d7
d3x0r <d3x0r@no-reply.github.com>
2020-02-25 10:19:58 -0800

    Some reformats and local prevent re-duplication of setting config

--- tests/objstore/testFileInlay.js
+++ tests/objstore/testFileInlay.js
@@ -18,57 +18,71 @@ const store = sack.ObjectStorage( vfs, "storage.os" );
 // consts...
 const storage = {
 	rootDirectory : null,
-	config : null,
+	config : {defaults:false},
 	configFile : null,
 };
 const waiters = [];
+const waiters2 = [];
 
 
 function loadConfig() {
-return new Promise( (res,rej)=>{
-	store.getRoot().then( directory=>{
-		// store.root is also set at this time.
-		function setConfig(data){
-			storage.config = sack.JSOX.parse( data );
+	if( waiters.length > 0 ) {
+		return new Promise( (res,rej)=>waiters.push( {res:res,rej:rej} ) );
+	}
 
-	 		if( !storage.config ){
-				console.log( "No configuration found, still, write new defaults" );
-				storage.config = {
-					defaults:true
-				};
-				saveConfig();
+	return new Promise( (res,rej)=>{
+		waiters.push( {res:res,rej:rej} );
+		store.getRoot().then( directory=>{
+			// store.root is also set at this time.
+			if( storage.configFile ) {
+				console.log( "ping" );
+				res( storage.config );
 			}
-			console.log( "And finally config:", storage.config );
-			res();
-		}
-		directory.open( "config.jsox" ).then( f=>{
-			storage.configFile = f;
-			storage.configFile.read().then( setConfig ).catch( ()=>{
-				saveConfig().then( ()=>{
-					storage.configFile.read().then( setConfig );
+			function setConfig(data){
+				storage.config = data;
+	        
+		 		if( !storage.config ){
+					console.log( "No configuration found, still, write new defaults" );
+					storage.config = {
+						defaults:true
+					};
+					saveConfig();
+				}
+				console.log( "And finally config:", storage.config );
+				waiters.forEach(w=>w.res(storage.config));
+				waiters.length = 0;
+			}
+			directory.open( "config.jsox" ).then( f=>{
+				storage.configFile = f;
+				storage.configFile.read().then( setConfig ).catch( ()=>{
+					saveConfig().then( ()=>{
+						storage.configFile.read().then( setConfig );
+					} );
 				} );
 			} );
 		} );
-	} );
-});
+	});
 
 }
 
 async function saveConfig() {
 	console.log( "Storage.config:", storage.config );
-	return storage.configFile.write( sack.JSOX.stringify( storage.config ) );
+	return storage.configFile.write( storage.config );
 }
 
 function getConfig() {
-	if( storage.config )
+	if( storage.configFile )
 		return Promise.resolve( storage.config );
+
 return new Promise( (res,rej)=>{
-	waiters.push( res );
-	loadConfig().then( ()=>{
-		Object.freeze( storage );
-		waiters.forEach( w=>w( storage.config ) );
-		waiters.length = 0;
-	} )
+	waiters2.push( res );
+
+	if( waiters2.length == 1 )	
+		loadConfig().then( ()=>{
+			Object.freeze( storage );
+			waiters2.forEach( w=>w( storage.config ) );
+			waiters2.length = 0;
+		} )
 } );
 }
 
