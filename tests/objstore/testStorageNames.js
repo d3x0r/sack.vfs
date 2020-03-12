6c210004d0d65cb5a5e9e5c5bcde7ef2b1cfa131
d3x0r <d3x0r@no-reply.github.com>
2020-03-11 18:44:34 -0700

    Commit debug in progress - timeline sector overflow seek error

--- tests/objstore/testStorageNames.js
+++ tests/objstore/testStorageNames.js
@@ -1,6 +1,6 @@
 
 const sack = require( "../.." );
-//sack.Volume().unlink( "storage.os" );
+sack.Volume().unlink( "storage.os" );
 
 const store = sack.ObjectStorage( "storage.os" );
 
@@ -9,8 +9,14 @@ var object = {
         get date() { return new Date()  }
 };
 
-for( var n = 0; n < 5; n++ )
+var ids = [];
+var nMax = Number( process.argv[2] );
+console.log( "input?", process.argv );
+if( nMax === NaN || !nMax ) nMax = 5;
+for( var n = 0; n < nMax; n++ )
 	store.put( Object.assign({},object) ).then( (id)=>{
+		if( !ids.length )
+			setTimeout( ()=>{ ids.sort( (a,b)=>a<b ); ids.forEach( id=>store.delete( id ) ); }, 1000 );
+		ids.push(id);
 		console.log( "ID:", id );
-		setTimeout( ()=>{ store.delete( id ); }, 1000 );
 	} );
