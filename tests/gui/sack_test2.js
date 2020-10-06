e803164413e7a355964f091ab9e1cb4fa49ea7b0
d3x0r <d3x0r@no-reply.github.com>
2020-10-06 05:35:47 -0700

    Update test to use an iamge that exists

--- tests/gui/sack_test2.js
+++ tests/gui/sack_test2.js
@@ -1,7 +1,7 @@
 var sack = require( "../.." );
 console.log( "Creates a simple output window and draws an image to it." );
 var r = sack.Renderer( "test", -1, -1, 500, 500 );
-var background = sack.Image( "the rror.jpg" );
+var background = sack.Image( "test-frame.png" );
 
 r.setDraw( ( image )=>{	
         image.drawImage( background );
