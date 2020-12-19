70144ff5ba332afeaafed2e5a5574e7844aa943a
d3x0r <d3x0r@no-reply.github.com>
2020-12-19 00:11:40 -0800

    Add test filler code

--- test/jsox/1.2.103.js
+++ test/jsox/1.2.103.js
@@ -2,6 +2,22 @@
 const SACK=require("../.." );
 const JSOX = SACK.JSOX;
 
+/*
+process.on("beforeExit", ()=>{ console.log( "EXITING" ) } );
+process.on("uncaughtException",(a,b)=>{
+	console.log( "test", a, b );
+} );
+
+function describe(a,b) { console.log( "doing:", a ); return b() };
+function it(a,b) { console.log( "test:", a ); return b() };
+let threw = null;
+function expect(a) { if( "function" === typeof a ) { try { threw = null; a(); } catch(err){threw=err}; }
+					 return ({ to: {deep:{  equal(b) { console.log( "did",a,"=",b);  } }
+					, equal(b) { console.log( "did",a,"=",b); }
+				  , throw(a) {console.log( "Success:error?", threw ) } } }); }
+*/
+
+
 describe('Added in 1.2.103', function () {
 
 
