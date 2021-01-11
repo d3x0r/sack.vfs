fafcd41eb9b8ddeb8e72e2b2bc2eb6486bec6ae0
d3x0r <d3x0r@no-reply.github.com>
2021-01-10 18:20:18 -0800

    Added more tests

---
+++ test/jsox/1.2.107-base64.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const SACK=require("../.." );
+const JSOX = SACK.JSOX;
+
+describe('Added in 1.2.107 - base64', function () {
+	it('quotes numeric base64', function() {
+			const ab = JSOX.parse( "ab['3abc']" );
+			const str = JSOX.stringify( ab );
+			expect( str).to.equal( "ab[\"3abc\"]" );
+
+	} );
+	it('skips quotes on alpha base64', function() {
+			const ab = JSOX.parse( "ab['a3bc']" );
+			const str = JSOX.stringify( ab );
+			expect( str).to.equal( "ab[a3bc]" );
+
+	} );
+} );
