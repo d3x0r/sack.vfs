9a27fe145ec89565d580e69407d04524efa8f62a
d3x0r <d3x0r@no-reply.github.com>
2021-01-18 20:30:53 -0800

    Fix incomplete escape test for this version

--- test/jsox/testIncompleteStringEscapes.js
+++ test/jsox/testIncompleteStringEscapes.js
@@ -8,26 +8,41 @@ const parse = JSOX.parse;
 
 let o;
 
+/*
+process.on("beforeExit", ()=>{ console.log( "EXITING" ) } );
+process.on("uncaughtException",(a,b)=>{
+	console.log( "test", a, b );
+} );
+
+function describe(a,b) { console.log( "doing:", a ); return b() };
+function it(a,b) { console.log( "test:", a ); return b() };
+let threw = null;
+function expect(a) { if( "function" === typeof a ) { try { threw = null; a(); } catch(err){threw=err}; }
+					 return ({ to: {deep:{  equal(b) { console.log( "did",a,"=",b);  } }
+					, equal(b) { console.log( "did",a,"=",b); }
+				  , throw(a) {console.log( "Success:error?", threw ) } } }); }
+
+*/
+
 describe('Incomplete String Escape tests', function () {
 
-	expect(function () {
-		o = parse( "'\\x1'" );
-		console.log( "got back:", o );
-	}).to.throw(Error);
+
 
 	it('Parses string octal escape followed by character', function () {
-		const result = JSON6.parse( '"\\012"' );
+		const result = JSOX.parse( '"\\012"' );
 		expect(result).to.equal('\0' + '12');
-	});
+	expect(parse( "'\\x1'" )).to.equal( "\x01" );
 
-	expect(function () {
+	expect( parse( "'\\u31'" ) )
+
+/*
+function () {
 		o = parse( "'\\u31'" );
 		console.log( "got back:", o );
-	}).to.throw(Error);
+	})*/.to.equal( "\u0031" );//throw(Error);
 
-	expect(function () {
-		o = parse( "'\\u{0'" );
-		console.log( "got back:", o );
-	}).to.throw(Error);
+	expect( parse( "'\\u{0'" )).to.equal("\0");
 
+
+	});
 } );
