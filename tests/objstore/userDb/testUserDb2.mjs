fa876f9b0b15a7436841283fd91e8d59506bbd02
d3x0r <d3x0r@no-reply.github.com>
2021-02-03 10:27:03 -0800

    Allow more ids to be reuqested on command line

--- tests/objstore/userDb/testUserDb2.mjs
+++ tests/objstore/userDb/testUserDb2.mjs
@@ -2,6 +2,14 @@
 import {User,go} from "./userDb.mjs"
 
 function getUsers() {
+	for( var n = 2; n < process.argv.length; n++ ) {
+		const num = Number(process.argv[n]);
+		
+		console.log( "Try to get:", num, process.argv[n] );
+		User.get( num ).then( (user)=>{
+			console.log( `Got ${num}:`, user );
+		} );
+	}
 	console.log( "Try to get:", 1523 );
 	User.get( 1523 ).then( (user)=>{
 		console.log( "Got 1523:", user );
