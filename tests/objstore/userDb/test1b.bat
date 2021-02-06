78aee796833bc65c0dbbcc8b91948556a6e52e4f
d3x0r <d3x0r@no-reply.github.com>
2021-02-06 10:58:22 -0800

    update test scripts to facilitate testing

--- tests/objstore/userDb/test1b.bat
+++ tests/objstore/userDb/test1b.bat
@@ -1,66 +1,75 @@
+set NODE=M:\node\debug\node.exe
+:set NODE=node.exe
+
 del data.os
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 copy data.os data.good
 time /T
 :15000
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
+copy data.os data.good.1
 time /T
 :30000
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
+copy data.os data.good.2
 time /T
 :45000
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
+copy data.os data.good.3
 time /T
 :60000
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
+copy data.os data.good.4
 time /T
 :75000
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
+copy data.os data.good.5
 time /T
 :90000
-node.exe testUserDb-auto.mjs 1  15000
-copy data.os data.os.good.5
+%NODE% testUserDb-auto.mjs 1  15000
+copy data.os data.good.6
 time /T
 :105000
 :M:\node\debug\node.exe --inspect-brk testUserDb-auto.mjs 1  15000
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
+exit
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
 time /T
-node.exe testUserDb-auto.mjs 1  15000
+%NODE% testUserDb-auto.mjs 1  15000
