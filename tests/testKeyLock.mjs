f3becc5a7619d71607a883a8547a434d69f90bbf
d3x0r <d3x0r@no-reply.github.com>
2021-05-23 20:37:06 -0700

    Add test

--- tests/testKeyLock.mjs
+++ tests/testKeyLock.mjs
@@ -7,8 +7,15 @@ keyboard.lock( true );
 
 console.log( "Can you type ANYTHIGN right now?" );
 
+// doesn't clear for garbage collection
+keyboard.onKey( ( key )=>{
+	console.log( "key event got key:", key );
+        return false;
+} );
+
 setTimeout( ()=>{
 	keyboard.lock( false );
 	console.log( "What about Now?" );
-        keyboard.onRead( null );
+        // don't get this callback, but should now garbage collect.
+        keyboard.onKey( null, true );
 }, 5000 );
