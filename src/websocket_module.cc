1f097fc3ed8c9b0f901acd69cd93bb99d5a53217
d3x0r <d3x0r@no-reply.github.com>
2021-05-28 17:45:27 -0700

    Fix compat with version12

--- src/websocket_module.cc
+++ src/websocket_module.cc
@@ -1846,7 +1846,7 @@ void httpObject::end( const v8::FunctionCallbackInfo<Value>& args ) {
 			contentLen = bodybuf->ByteLength();
 			//VarTextAddData( obj->pvtResult, (CTEXTSTR)bodybuf->GetBackingStore()->Data(), bodybuf->ByteLength() );
 #else
-			content = bodybuf->GetContents()->Data();
+			content = bodybuf->GetContents().Data();
 			contentLen = bodybuf->ByteLength();
 			//VarTextAddData( obj->pvtResult, (CTEXTSTR)bodybuf->GetContents().Data(), bodybuf->ByteLength() );
 #endif
@@ -3318,8 +3318,14 @@ void httpRequestObject::getRequest( const FunctionCallbackInfo<Value>& args, boo
 		if( content->IsArrayBuffer() ) {
 			// zero copy method... content 
 			Local<ArrayBuffer> ab = Local<ArrayBuffer>::Cast( args[0] );
+
+#if ( NODE_MAJOR_VERSION >= 14 )
 			httpRequest->content = (char*)ab->GetBackingStore()->Data();
 			httpRequest->contentLen = ab->GetBackingStore()->ByteLength();
+#else
+			httpRequest->content = (char*)ab->GetContents().Data();
+			httpRequest->contentLen = ab->ByteLength();
+#endif
 
 		} else {
 			// converted from JS widechar to utf8... (transform+copy)
