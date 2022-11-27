5e21c474b63110b80de430807b26b94147d893ee
d3x0r <d3x0r@no-reply.github.com>
2022-11-27 14:20:46 -0800

    Add tenative support for loading in electron

--- vfs_module.cjs
+++ vfs_module.cjs
@@ -10,7 +10,27 @@ try {
 }
 
 if( !sack )
-  if( process.platform === 'win32' ) {
+  if( process.platform === 'browser' ) { // electron
+      try {
+          sack = require( "./build/Release/sack_vfs.node" );
+			console.log( "Release loaded Yes.", sack );
+      } catch( err ){
+        errN.push(err);
+      }
+		if( !sack )
+          try {
+              sack = require( "./build/Debug/sack_vfs.node" );
+				console.log( "Debug loaded Yes.", sack );
+          } catch( err ){
+            errN.push(err);
+          }
+		if( !sack )
+          try {
+              sack = require( "./build/RelWithDebInfo/sack_vfs.node" );
+          } catch( err ){
+            errN.push(err);
+          }
+  } else if( process.platform === 'win32' ) {
     try {
       if( process.config.target_defaults.default_configuration === 'Debug' 
 		|| ( Number(process.version.split('.')[0].split('v')[1]) >= 16 ) 
@@ -26,6 +46,7 @@ if( !sack )
       } catch( err ){
         errN.push(err);
       }
+		if( !sack )
       try {
           sack = require( "./build/Release/sack_vfs.node" );
       } catch( err ){
@@ -52,15 +73,15 @@ if( !sack )
         errN.push(err);
       }
   }
-
 if( !sack )
-  throw new Error( util.format( "Failed to match configuration:", process.config.target_defaults.default_configuration, "\n", errN.join(',') ) );
+  throw new Error( util.format( "Failed to match configuration:", process && process.config && process.config.target_defaults && process.config.target_defaults.default_configuration, "\n", errN.join(',') ) );
 
 require( "./sack-jsox.cjs" )(sack);
 require( "./object-storage.cjs" )(sack);
 require( "./object-storage-cb.cjs" )(sack);
 
 module.exports=exports=sack;
+
 
 // needed to dispatch promise resolutions that have been created in callbacks.
 if (process._tickDomainCallback || process._tickCallback)
