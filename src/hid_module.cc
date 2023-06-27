b5cbaf936c548c20dda4473f585b623b2d32fbb1
d3x0r <d3x0r@no-reply.github.com>
2023-06-27 16:41:27 -0700

    Remove a lot of dead code

--- src/hid_module.cc
+++ src/hid_module.cc
@@ -8,8 +8,6 @@
 
 static uintptr_t InputThread( PTHREAD thread );
 static void CPROC dispatchKey( uintptr_t psv, RAWINPUT *event, WCHAR ch, int len );
-#define WM_HOOK2 WM_USER+512
-#define WM_HOOK3 WM_USER+513
 
 class KeyHidObject : public node::ObjectWrap {
 public:
@@ -67,6 +65,7 @@ struct input_data {
 struct msgbuf {
 	LOGICAL close;
 	RAWINPUT event;
+	KBDLLHOOKSTRUCT hookEvent;
 	WCHAR ch;
 };
 
@@ -100,240 +99,6 @@ typedef struct global_tag
 
 static GLOBAL hidg;
 
-static RAWINPUTDEVICE devices[] = {
-	{ 1, 6, RIDEV_NOLEGACY | RIDEV_INPUTSINK,  0 }
-};
-
-LRESULT CALLBACK WndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam )
-{
-	UINT uSize;
-
-	switch( uMsg ) {
-	case WM_CREATE:
-		/* 1 - generic desktop controls // we use this
-		2 - simulation controls
-		3 - vr
-		4 - sport
-		5 - game 
-		6 - generic device
-		7 - keyboard
-		8 - LEDs
-		9 - button
-		*/
-		/*
-		// for page 1 (above)
-		0 - undefined
-		1 - pointer
-		2 - mouse
-		3 - reserved
-		4 - joystick
-		5 - game pad
-		6 - keyboard // we use this
-		7 - keypad
-		8 - multi-axis controller
-		9 - Tablet PC controls
-		*/
-		devices[0].hwndTarget = hWnd;
-		int rc;
-		rc = RegisterRawInputDevices( devices, 1, sizeof( RAWINPUTDEVICE ) );
-#if 0
-		RAWINPUTDEVICELIST *devices;
-		UINT uSize; uSize = sizeof( devices );
-		UINT nDev;
-		UINT n;
-		nDev = 0;
-		/*
-		RAWINPUTDEVICE *devices2;
-		GetRegisteredRawInputDevices( NULL, &nDev, sizeof( RAWINPUTDEVICE ) );
-		devices2 = NewArray( RAWINPUTDEVICE, nDev );
-		GetRegisteredRawInputDevices( devices2, &nDev, sizeof( RAWINPUTDEVICE ) );
-		*/
-
-		GetRawInputDeviceList( NULL, &nDev, sizeof( RAWINPUTDEVICELIST ) );
-		lprintf( "%d", GetLastError());
-		devices = NewArray( RAWINPUTDEVICELIST, nDev );
-		GetRawInputDeviceList( devices, &nDev, sizeof( RAWINPUTDEVICELIST ) );
-		lprintf( "%d", GetLastError() );
-		for( n = 0; n < nDev; n++ ) {
-			struct input_data *indev;
-			UINT uSize;
-			indev = NewArray( struct input_data, 1 );
-			indev->hDevice = devices[n].hDevice;
-			GetRawInputDeviceInfo(
-				devices[n].hDevice,
-				RIDI_DEVICENAME,
-				NULL,
-				&uSize
-			);
-			indev->name = NewArray( char, uSize );
-			GetRawInputDeviceInfo(
-				devices[n].hDevice,
-				RIDI_DEVICENAME,
-				indev->name,
-				&uSize
-			);
-			lprintf( "New Device: %s", indev->name );
-			AddLink( &hidg.inputs, indev );
-		}
-#endif
-		//SetTimer( hWnd, 100, 1000, NULL );
-		return TRUE;
-	case WM_HOOK2:
-		//lprintf( "HOOK2 MSG" );
-		return TRUE;;;;;;
-	case WM_HOOK3:
-		//lprintf( "HOOK3 MSG" );
-		return TRUE;;;;;;
-	case WM_INPUT:
-		if( GetRawInputData( (HRAWINPUT)lParam,
-			RID_INPUT, NULL, &uSize, sizeof( RAWINPUTHEADER ) ) == -1 ) {
-			break;
-		}
-		LPBYTE lpb = new BYTE[uSize];
-		if( lpb == NULL ) {
-			break;
-		}
-		if( GetRawInputData( (HRAWINPUT)lParam,
-			RID_INPUT, lpb, &uSize, sizeof( RAWINPUTHEADER ) ) != uSize ) {
-			delete[] lpb;
-			break;
-		}
-		RAWINPUT *input = (RAWINPUT *)lpb;
-#if 0
-#define RI_KEY_MAKE             0
-#define RI_KEY_BREAK            1
-#define RI_KEY_E0               2
-#define RI_KEY_E1               4
-#define RI_KEY_TERMSRV_SET_LED  8
-#define RI_KEY_TERMSRV_SHADOW   0x10
-#endif
-
-		UINT Event;
-		WCHAR keyChar;
-
-		Event = input->data.keyboard.Message;
-		keyChar = MapVirtualKey( input->data.keyboard.VKey, MAPVK_VK_TO_CHAR );
-		{
-			INDEX idx;
-			struct input_data *indev;
-			LIST_FORALL( hidg.inputs, idx, struct input_data *, indev ) {
-				if( indev->hDevice == input->header.hDevice )
-					break;
-			}
-			if (!indev) {
-				indev = NewArray(struct input_data, 1);
-				memset(indev, 0, sizeof(*indev));
-				indev->hDevice = input->header.hDevice;
-				if (!input->header.hDevice) {
-					indev->name = StrDup( "Stdin" );
-
-				}
-				else {
-					UINT uSize;
-					UINT x = GetRawInputDeviceInfo(
-						input->header.hDevice,
-						RIDI_DEVICENAME,
-						NULL,
-						&uSize
-					);
-					DWORD dwError = GetLastError();
-					if (dwError == ERROR_INVALID_HANDLE)
-					{
-						lprintf("result: %d %d %p", x, GetLastError(), input->header.hDevice);
-						//	ERROR_SUCCESS
-					}
-					else {
-						indev->name = NewArray(char, uSize);
-						GetRawInputDeviceInfo(
-							input->header.hDevice,
-							RIDI_DEVICENAME,
-							indev->name,
-							&uSize
-						);
-						//if(0)
-
-					}
-				}
-				//lprintf("New Device: %s", indev->name);
-				AddLink(&hidg.inputs, indev);
-			}
-		}
-		if( hidg.eventHandler )
-			dispatchKey( (uintptr_t)hidg.eventHandler
-				, input, keyChar, 0 );
-
-		if(0)
-		LoG( "Got: %c(%d) %p %d %d %d %d %d %d %d ", keyChar,keyChar
-			, input->header.hDevice
-			, input->header.wParam
-			, input->data.keyboard.Reserved, input->data.keyboard.ExtraInformation,
-			input->data.keyboard.MakeCode,
-			input->data.keyboard.Flags,
-			input->data.keyboard.VKey,
-			input->data.keyboard.Message );
-
-		delete[] lpb;			// free this now
-			
-		return TRUE;
-	}
-	return DefWindowProc( hWnd, uMsg, wParam, lParam );
-}
-
-int MakeProxyWindow( void )
-{
-	ATOM aClass;
-	{
-		WNDCLASS wc;
-		memset( &wc, 0, sizeof( WNDCLASS ) );
-		wc.style = CS_OWNDC | CS_GLOBALCLASS;
-
-		wc.lpfnWndProc = (WNDPROC)WndProc;
-		wc.hInstance = GetModuleHandle( NULL );
-		wc.hbrBackground = 0;
-		wc.lpszClassName = "sack.vfs.raw.input.receiver.class";
-		aClass = RegisterClass( &wc );
-		if( !aClass ) {
-			MessageBox( NULL, "Failed to register class to handle SQL Proxy messagses.", "INIT FAILURE", MB_OK );
-			return FALSE;
-		}
-	}
-
-	hidg.hWnd = CreateWindowEx( 0,
-		(char*)aClass,
-		"sack.vfs.raw.input.receiver",
-		0,
-		0,
-		0,
-		0,
-		0,
-		HWND_MESSAGE, // Parent
-		NULL, // Menu
-		GetModuleHandle( NULL ),
-		(void*)1 );
-	if( !hidg.hWnd ) {
-		Log( "Failed to create window!?!?!?!" );
-		MessageBox( NULL, "Failed to create window to handle raw input Messages", "INIT FAILURE", MB_OK );
-		return FALSE;
-	}
-	return TRUE;
-}
-
-LRESULT WINAPI KeyboardProc( int code, WPARAM wParam, LPARAM lParam ) {
-	KBDLLHOOKSTRUCT *kbhook = (KBDLLHOOKSTRUCT*)lParam;
-	lprintf( "   keyhook for key... %08x  %d %d %x", wParam, kbhook->scanCode, kbhook->vkCode, kbhook->flags );
-	MSG msg;
-	while( PeekMessage( &msg, NULL, 0, 0, PM_REMOVE ) )  { DispatchMessage( &msg );  lprintf( "had message..." ); }
-	return CallNextHookEx( hidg.hookHandle, code, wParam, lParam );
-}
-
-LRESULT WINAPI MouseProc( int code, WPARAM wParam, LPARAM lParam ) {
-	MOUSEHOOKSTRUCT *mhook = (MOUSEHOOKSTRUCT*)lParam;
-	lprintf( "   mousehook... %08x  %d %d %x", wParam, mhook->pt.x, mhook->pt.y, mhook->wHitTestCode, mhook->dwExtraInfo );
-	MSG msg;
-	while( PeekMessage( &msg, NULL, 0, 0, PM_REMOVE ) ) { DispatchMessage( &msg );   lprintf( "had message..." ); }
-	return CallNextHookEx( hidg.hookHandleM, code, wParam, lParam );
-}
-
 
 LRESULT WINAPI KeyboardProcLL( int code, WPARAM wParam, LPARAM lParam ) {
 	KBDLLHOOKSTRUCT *kbhook = (KBDLLHOOKSTRUCT*)lParam;
@@ -353,26 +118,11 @@ LRESULT WINAPI KeyboardProcLL( int code, WPARAM wParam, LPARAM lParam ) {
 	//static 
 	int n = 10;
 	int up = (kbhook->flags & LLKHF_UP) != 0;
+
 	if( resending ) {
 		return CallNextHookEx( hidg.hookHandleLL, code, wParam, lParam );
 	}
-	if( up ) {
-		if( lastDownSkip ) {
-			States[n].eventsUp[States[n].state].type = INPUT_KEYBOARD;
-			States[n].eventsUp[States[n].state].ki.dwExtraInfo = kbhook->dwExtraInfo;
-			States[n].eventsUp[States[n].state].ki.dwFlags = kbhook->flags;
-			States[n].eventsUp[States[n].state].ki.time = kbhook->time;
-			States[n].eventsUp[States[n].state].ki.wScan = (WORD)kbhook->scanCode;
-			States[n].eventsUp[States[n].state].ki.wVk = (WORD)kbhook->vkCode;
-			lastDownSkip--;
-			return 1;
-		}
-		else {
-			return CallNextHookEx( hidg.hookHandleLL, code, wParam, lParam );
-		}
-	}
-
-	if(	hidg.blocking ) {
+	if( hidg.blocking ) {
 		hidg.skipEvent = 1;
 		if( 0 ) {
 			LoG( "Drop key." );
@@ -380,11 +130,20 @@ LRESULT WINAPI KeyboardProcLL( int code, WPARAM wParam, LPARAM lParam ) {
 		}
 		return 1;
 	}
-	//SendMessage( hidg.hWnd, WM_HOOK2, 0, 0 );
-	//LoG( "LL keyhook for key... %08x  %d %d %x", wParam, kbhook->scanCode, kbhook->vkCode, kbhook->flags );
-	//MSG msg;
-	//while( PeekMessage( &msg, NULL, 0, 0, PM_REMOVE ) ) { DispatchMessage( &msg );  lprintf( "had LL message..." ); }
+
+	char ch = MapVirtualKey( kbhook->vkCode, MAPVK_VK_TO_CHAR );
+	struct msgbuf* msgbuf = NewPlus( struct msgbuf, 0 );
+	msgbuf->close = FALSE;
+	//msgbuf->event = event[0];
+	msgbuf->hookEvent = kbhook[0];
+	msgbuf->ch = ch;
+
+	//KeyHidObject* com = (KeyHidObject*)psv;
+	EnqueLink( &hidg.eventHandler->readQueue, msgbuf );
+	uv_async_send( &hidg.eventHandler->async );
+
 	return CallNextHookEx( hidg.hookHandleLL, code, wParam, lParam );
+
 }
 
 LRESULT WINAPI MouseProcLL( int code, WPARAM wParam, LPARAM lParam ) {
@@ -412,12 +171,9 @@ uintptr_t InputThread( PTHREAD thread )
 	//WH_KEYBOARD_LL
 	HMODULE xx;
 	xx = GetModuleHandle( "sack_vfs.node" );
-	//lprintf( "%p", xx );
 	hidg.hookHandleLL = SetWindowsHookEx( WH_KEYBOARD_LL, (HOOKPROC)KeyboardProcLL, xx, 0 );
-	hidg.hookHandle = SetWindowsHookEx( WH_KEYBOARD, KeyboardProc, xx, 0 );
-	//lprintf( "hook:%p %d", hidg.hookHandle, GetLastError() );
 	hidg.nWriteTimeout = 150; // at 9600 == 144 characters
-	if( MakeProxyWindow() ) {
+	{
 		MSG msg;
 		//lprintf( "Err:%d",GetLastError());
 		while( GetMessage( &msg, NULL, 0, 0 ) )
