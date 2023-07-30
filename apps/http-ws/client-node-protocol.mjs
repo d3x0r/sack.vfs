43ce7dc2151690dc83753c3cbaabc807fd9040eb
d3x0r <d3x0r@no-reply.github.com>
2023-07-29 19:31:39 -0700

    Modify node client somewhat

--- apps/http-ws/client-node-protocol.mjs
+++ apps/http-ws/client-node-protocol.mjs
@@ -8,33 +8,30 @@ export class Protocol extends Events {
 	static debug = true;
 	ws = null;
 	protocol = null;
-	constructor( protocol ){
+	constructor( address, protocol ){
 		super();
+		this.address = address;
 		this.protocol = protocol;
 		if( protocol )
-			Protocol.connect(protocol, this);
+			this.connect(protocol, this);
 	}
 	set debug(val) { Protocol.debug = val; }
 	get debug() { return Protocol.debug; }
 
-	static connect(protocol, this_) {
+	connect(protocol, this_) {
 		if( this.ws ) this.ws.close( 1006, "close duplicate socket" );
-		this.ws = new sack.WebSocketClient( location.origin.replace("http","ws"), protocol );
-		this.ws.onmessage = (evt)=>Protocol.onmessage.call( this_, evt) ;
-		this.ws.onclose = (evt)=>Protocol.onclose.call( this_, evt) ;
-		this.ws.onopen = (evt)=>Protocol.onopen.call( this_, evt) ;
+		this.ws = new sack.WebSocketClient( this.address, protocol );
+		this.ws.onmessage = (msg)=>this.onmessage(msg) ;
+		this.ws.onclose = (code,reason)=>this.onclose(code,reason) ;
+		this.ws.onopen = (msg)=>this.onopen.call( evt) ;
 		return this.ws;
 	}
 	
-	connect() {
-		return this.connect( this.protocol, this );
-	}
-
-	static onopen( evt ) {
+	onopen( evt ) {
 		this.on( "open", true );
 	}
 
-	static onclose( evt ){
+	onclose( evt ){
 		Protocol.debug && console.log( "close?", this, evt );
 		this.on( "close", [evt.code, evt.reason] );
 		this.ws = null;
@@ -42,7 +39,7 @@ export class Protocol extends Events {
 		else setTimeout( this.connect, 5000 );
 	}
 
-	static onmessage( evt ) {
+	onmessage( evt ) {
 		Protocol.debug && console.log( "got:", this, evt );
 		const msg = JSOX.parse( evt.data );
 		if( !this.on( msg.op, msg ) ){
