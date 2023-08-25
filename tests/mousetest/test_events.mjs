e9c9d528ed3e460da59f73ca262d75385f94b364
d3x0r <d3x0r@no-reply.github.com>
2023-08-24 19:35:14 -0700

    Update test

--- tests/mousetest/test_events.mjs
+++ tests/mousetest/test_events.mjs
@@ -1,24 +1,32 @@
 import {sack} from "sack.vfs"
 
+
+        sack.Mouse.event( 960, 540, 0 );
+	console.log( "Mouse Moved  to:", sack.Mouse.cursor );
+
+/*
 try {
 	sack.Mouse.event( 10, 10 );
         console.log( "This should have thrown an error, and is an error if you see this." );
 } catch( err) {
 	console.log( "Expected Error:", err );
 }
-
+*/
 function g( i ) {
 	if( i > 1000 ) return;
 	if( !i )
 	        sack.Mouse.event( 1, 1, 0 );
-	else
+	else {
 	        sack.Mouse.event( 10, 10, sack.Mouse.buttons.relative );
+	}
 	setTimeout( ()=>g(i+10), 50 );
 }
 
 function f( i ) {
-	if( i > 1000 ) return g(0);
-        sack.Mouse.event( i, i, 0 );
+	if( i > 1000 ) return ;//g(0); 
+	console.log( "Mouse starts at:", sack.Mouse.cursor );
+        sack.Mouse.event( i*4, i*2, 0 );
+	console.log( "Mouse Moved  to:", sack.Mouse.cursor );
 	setTimeout( ()=>f(i+10), 50 );
 }
 
