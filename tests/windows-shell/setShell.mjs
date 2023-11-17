e53abbd7dbcbe2ca0c398beb58bc5c3029746312
d3x0r <d3x0r@no-reply.github.com>
2023-11-16 17:45:41 -0800

    use admin flag

--- tests/windows-shell/setShell.mjs
+++ tests/windows-shell/setShell.mjs
@@ -1,7 +1,7 @@
 import {sack} from "sack.vfs"
 const status = sack.system.setShell( "explorer.exe" );
 if( !status ) {
-	sack.Task( { bin:process.argv[0], args:"elevateUtil.mjs " + process.argv[1], end() {} } );
+	sack.Task( { bin:process.argv[0], args:process.argv[1], admin:true, end() {} } );
 }
 console.log( "Set Explorer worked?", status );
 setTimeout( ()=>{}, 5000 );
