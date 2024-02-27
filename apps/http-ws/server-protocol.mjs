9a7b10848b688436dd5796a3a5f9a5d39e741bea
d3x0r <d3x0r@no-reply.github.com>
2024-02-26 17:06:00 -0800

    Add some documentation and improve some event dispatch

--- apps/http-ws/server-protocol.mjs
+++ apps/http-ws/server-protocol.mjs
@@ -12,15 +12,18 @@ function loopBack( that, to ) {
 }
 
 export class Protocol extends Events {
-
 	protocol = null;
 	server = null;
+	/** 
+	 * @param {object} opts - options for the protocol
+	 */ 
 	constructor( opts ) {
 		super();
 		// resource path is from current working directory (where it ran from)
 		if( opts && opts.protocol ) Protocol.protocol = opts.portocol;
 		this.server = openServer( opts || { resourcePath:"ui", port:Number(process.env.PORT)||4321 }
 					, loopBack( this, this.#accept ), loopBack( this, this.#connect ) );
+		//this.on( "close", )
 	}
 
 	#accept( server, ws ) {
@@ -52,19 +55,21 @@ export class Protocol extends Events {
 
 		ws.on("message", handleMessage);
 		ws.on("close", handleClose );
+
 		const parser = sack.JSOX.begin( 
-			(object)=>Protocol.dispatchMessage(this_, myWS,object) );
+			(object)=>Protocol.#dispatchMessage(this_, myWS,object) );
+
 		function handleClose( code, reason ) {
-			this_.on( "close", [code,reason] );
+			this_.on( "close", [myWS,code,reason] );
 		}
 
 		function handleMessage( msg ) {
 			const result = this_.on( "message", [ws,msg])
-			if( !result || ! result.reduce( (acc,val)=>acc|=val, false ) )
+			if( !result || ! result.reduce( (acc,val)=>acc|=!!val, false ) )
 				parser.write( msg );
 		}
 	}
-	static dispatchMessage(protocol, ws, msg ) {
+	static #dispatchMessage(protocol, ws, msg ) {
 		protocol.on( msg.op, [ws, msg] ); 
 	}
 }
@@ -74,11 +79,27 @@ class WS{
 	constructor(ws){
 		this.ws = ws;
 	}
+	/**
+	 * send a message - with automatic JSOX encoding if the message is an object.
+	 * @param {*} msg - message to send to the server, if an object, it will be sent as a JSOX object, otherwise it will be sent as a literal string.
+	 */
 	send( msg ) {
 		if( "object" === typeof msg ) 
 			this.ws.send( JSOX.stringify(msg) ); 
 		else
 			this.ws.send( msg );	
+	}
+	/**
+	 * emit an event to the server
+	 * @param {*} cmd - string command to genereate
+	 * @param {*} data - data to send with the command, if an object, it will be sent as a JSOX object,
+	 *                   otherwise it will be sent as a JSOX object with the key being the command.
+	 */
+	emit( cmd, data ){
+		if( "object" === typeof data ) {			
+			this.ws.send( JSOX.stringify(Object.assign( {op:cmd}, data )) );
+		} else
+			this.ws.send( JSOX.stringify({ op:cmd, [cmd]:data }) );
 	}
 }
 
