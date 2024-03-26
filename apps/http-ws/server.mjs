be3725bc2c042b4db6bdd10a808ff2ba84987e0a
d3x0r <d3x0r@no-reply.github.com>
2024-03-26 12:36:29 -0700

    Allow cors 404

--- apps/http-ws/server.mjs
+++ apps/http-ws/server.mjs
@@ -155,7 +155,7 @@ export function openServer( opts, cbAccept, cbConnect )
 			reqTimeout = setTimeout( logRequests, 100 );
 				
 			requests.push( "Failed request: " + req.url + " as " + lastFilePath );
-			res.writeHead( 404 );
+			res.writeHead( 404, {'Access-Control-Allow-Origin' : req.connection.headers.Origin } );
 			res.end( "<HTML><HEAD><title>404</title></HEAD><BODY>404<br>"+req.url+"</BODY></HTML>");
 		}
 	}
