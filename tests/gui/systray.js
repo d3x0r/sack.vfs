cc0da4fe6b04acd666436a31f1f1460426dd171e
d3x0r <d3x0r@no-reply.github.com>
2024-03-29 18:07:43 -0700

    Change changed icon

--- tests/gui/systray.js
+++ tests/gui/systray.js
@@ -1,7 +1,7 @@
 
 var sack = require( "../.." );//sack-gui" );
 
-sack.Systray.set( "pdown.ico", ()=>{
+sack.Systray.set( "sideplayR.ico", ()=>{
 	console.log( "Launch browser(double click)" );
         sack.Task( {
         	bin:"http://localhost:8123/index.html"
