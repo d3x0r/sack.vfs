238207d50966600211efdc7137de87910ec3164d
Jim B <d3x0r@users.noreply.github.com>
2024-03-29 17:23:45 -0700

    Reformat connect.mjs

--- tests/ssh/connect.mjs
+++ tests/ssh/connect.mjs
@@ -10,24 +10,24 @@ ssh.connect( { address:"localhost:2222", port:22
              , trace: false
              }
            ).then( (fingerprint)=>{
-					console.log( "Connect worked forssh",  fingerprint );
-					console.log( "Make channel1:" );
+                console.log( "Connect worked forssh",  fingerprint );
+                console.log( "Make channel1:" );
              	ssh.Channel( { type:"session", message: "pre-message", windowSize: 4096, packetSize: 4096 } ).then( (channel)=>{
-						console.log( "First channel result?", channel );
-					} );
+                   console.log( "First channel result?", channel );
+                } );
              	console.log( "Make channel2:" );
              	ssh.Channel( ).then( (channel)=>{
-						console.log( "Generic channel result:", channel );
-                	channel.read( (buf)=>{
-                        	console.log( "Channel data:", buf );
-                        } );
-                	channel.pty();
-                        channel.setenv( "FOO", "bar" );
-                        channel.shell().then( ()=>{
-                        	channel.send( "ls\n" );
-                        } );
+                   console.log( "Generic channel result:", channel );
+                   channel.read( (buf)=>{
+                      console.log( "Channel data:", buf );
+                   } );
+                   channel.pty();
+                   channel.setenv( "FOO", "bar" );
+                   channel.shell().then( ()=>{
+                      channel.send( "ls\n" );
+                   } );
                 } );
-             } );
+            } );
              
 if( false ) {
 	// test various address connection errors to be thrown.
