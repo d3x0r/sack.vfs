9309695208a0554ac1aeed47865289bd1d98c004
d3x0r <d3x0r@no-reply.github.com>
2024-03-29 18:04:34 -0700

    Add text to comment

--- tests/gui/systray.js
+++ tests/gui/systray.js
@@ -2,7 +2,7 @@
 var sack = require( "../.." );//sack-gui" );
 
 sack.Systray.set( "pdown.ico", ()=>{
-	console.log( "Launch browser" );
+	console.log( "Launch browser(double click)" );
         sack.Task( {
         	bin:"http://localhost:8123/index.html"
         } );
