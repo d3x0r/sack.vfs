e5cbcc42bbb0690c70751f56ba99c8df292e6eef
d3x0r <d3x0r@no-reply.githbu.com>
2024-05-11 23:44:57 -0400

    Fix dot squash

--- src/sack/sack.h
+++ src/sack/sack.h
@@ -10103,6 +10103,7 @@ using namespace sack::containers::text::http;
 #else
 #define WEBSOCKET_EXPORT IMPORT_METHOD
 #endif
+struct html5_web_socket;
 // the result returned from the web_socket_opened event will
 // become the new value used for future uintptr_t parameters to other events.
 typedef uintptr_t (*web_socket_opened)( PCLIENT pc, uintptr_t psv );
