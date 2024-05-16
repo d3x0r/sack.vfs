3c48663641528edfeb8692f6e9d53122e8398712
d3x0r <d3x0r@no-reply.githbu.com>
2024-05-16 02:14:15 -0400

    add debug script

---
+++ apps/task-manager/god.sh
@@ -0,0 +1,9 @@
+export DEBUG=*
+export NVM_DIR=$HOME/.nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm use 17
+#call npm run start
+gdb --args node  --experimental-loader=sack.vfs/import.mjs  src/main.mjs
+
+
+exec $*
