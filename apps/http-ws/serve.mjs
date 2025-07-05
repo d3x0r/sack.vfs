5211bb972365f20d82b5f5d684f74f163aee8bef
d3x0r <d3x0r@no-reply.github.com>
2025-07-04 23:04:36 -0700

    Only need to open the server once

--- apps/http-ws/serve.mjs
+++ apps/http-ws/serve.mjs
@@ -1,5 +1,4 @@
 import {openServer} from "./server.mjs"
 
-openServer(  );
-openServer( {address:"0.0.0.0" } );
+openServer( );
 console.log( "serving on?", Number(process.env.PORT) || (process.argv.length > 2?Number(process.argv[2]) : 8080) );
