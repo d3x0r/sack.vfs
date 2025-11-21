457fdd87b5d3f1b8978706168432dd418e5bda62
d3x0r <d3x0r@no-reply.github.com>
2025-11-21 00:15:37 -0800

    add comment

--- tests/keyboard/testPressKey.mjs
+++ tests/keyboard/testPressKey.mjs
@@ -2,18 +2,16 @@
 
 import {sack} from "sack.vfs"
 
-
+// test keboard input (get values for keys to use)
 const kb = sack.Keyboard( (key)=>{
 	//console.log( "Got key:", key );
-        return false; // return true to consume
+    return false; // return true to consume
 } );
 
 function k1() {
 kb.send( [{key:68,code:32,down:true,extended:false} ] ) //` to send key events.
 kb.send( [{key:68,code:32,down:false,extended:false} ] ) //` to send key events.
-
 setTimeout( k2, 8 );
-
 }
 
 function k2() {
@@ -21,9 +19,5 @@ kb.send( [{key:65,code:30,down:true,extended:false} ] ) //` to send key events.
 kb.send( [{key:65,code:30,down:false,extended:false} ] ) //` to send key events.
 setTimeout( k1, 8 );
 }
-
-//setTimeout( k1, 25 );
-
-//setTimeout( k2
 
 k1()
