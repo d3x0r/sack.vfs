0930d0f17bf5a7fbc79d8d0d2986c35e8a1d534e
Jim B <d3x0r@users.noreply.github.com>
2026-01-11 11:59:25 -0800

    Document ref class with usage examples
    Added detailed comments explaining the ref class and its usage.

--- apps/ref/ref.mjs
+++ apps/ref/ref.mjs
@@ -1,3 +1,27 @@
+// class that is a reference
+//   constructor parameters
+//     o - the object containing the field referenced
+//     f - the name of the field in the object (or number if o is an array)
+//     v - initial value to set the refernce to
+//     s - setter callback, when the value is updated, this callback is called
+//         with the value being set to the field.
+//
+//  It is possible to construct a reference that only contains the setter callback
+//  If the object is not passed, a default containing the field name is created
+//
+//  Usage:
+//    const local = { data : 3 };
+//    const dataRef = new ref( local, data );
+//
+//   dataRef['*'] = 3;   // using setter
+//   if( dataRef['*'] > 5 ) {   // using getter
+//      /* do something */
+//   }
+//
+//   const codeDataRef = new ref( null, null, null, (val)=>{
+//         local.data = val;
+//      } );
+
 
 export class ref { 	
 	#o = null;
@@ -12,3 +36,4 @@ export class ref {
 	get '*'(){ return this.#o[this.#f]; } 
 	set '*'(val){ this.#o[this.#f] = val; if( this.#s ) this.#s(val); }
 }
+
