7a7c5e26447d8e8aa989bdcea8dd9366614670a5
d3x0r <d3x0r@no-reply.github.com>
2026-02-19 02:43:47 -0800

    Add some better logging

--- tests/ssh/connect.mjs
+++ tests/ssh/connect.mjs
@@ -3,14 +3,14 @@ import {sack} from "sack.vfs"
 const ssh = sack.SSH();
 const ssh_alt = new sack.SSH();
 
-ssh.connect( { address:"localhost:2222", port:22
+ssh.connect( { address:"10.173.0.1", port:22
              , user:"root", password: "password"
              , privKey: null
              , pubKey:null  // pubKey can be gotten from the private key
              , trace: false
              }
            ).then( (fingerprint)=>{
-                console.log( "Connect worked forssh",  fingerprint );
+                console.log( "Connect worked forssh",  fingerprint, [...(new Uint8Array( fingerprint))].map(x=>x.toString(16).padStart(2,'0')).join('') );
                 console.log( "Make channel1:" );
              	ssh.Channel( { type:"session", message: "pre-message", windowSize: 4096, packetSize: 4096 } ).then( (channel)=>{
                    console.log( "First channel result?", channel );
@@ -27,7 +27,7 @@ ssh.connect( { address:"localhost:2222", port:22
                       channel.send( "ls\n" );
                    } );
                 } );
-            } );
+            } ).catch( (err)=>console.log( "Failed:", err ) );
              
 if( false ) {
 	// test various address connection errors to be thrown.
