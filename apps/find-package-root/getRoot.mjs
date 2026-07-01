e19053ac80c1a2d8b60b5f9607a1b3672a32509b
d3x0r <d3x0r@no-reply.github.com>
2026-07-01 09:45:41 -0700

    fix level comparison

--- apps/find-package-root/getRoot.mjs
+++ apps/find-package-root/getRoot.mjs
@@ -3,11 +3,11 @@ import {sack} from "sack.vfs"
 
 export function getRoot( min ) {
 	const disk = sack.Volume();
-	let levels = (typeof min === "Number")?min:0;
+	let levels = (typeof(min )=== "Number")?min:0;
 
 	do {
 		let s = '';
-		for( let l = 0; l < levels; l++ ) s += s?"/..":"..";
+		for( let l = 0; l <= levels; l++ ) s += s?"/..":"..";
 		if( !s ) s = '.';
 		const d = disk.dir( s, "package.json" );
       //console.log( "d?", s+ "/package.json", d );
