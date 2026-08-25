33f6adcc36602838809e03853c41ff405e9a4972
d3x0r <d3x0r@no-reply.github.com>
2026-08-25 04:17:30 -0700

    Protect against not having a first argument

--- apps/isTopLevel/isTopLevel.mjs
+++ apps/isTopLevel/isTopLevel.mjs
@@ -5,6 +5,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url'
 
-const nodePath = path.resolve(process.argv[1]);
+const nodePath = ( process.argv.length > 1 )?path.resolve(process.argv[1]):process.cwd();
 export const isTopLevel = ((testPath)=>(nodePath === path.resolve(fileURLToPath(testPath))));
 
